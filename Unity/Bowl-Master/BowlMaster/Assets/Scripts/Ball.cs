44af88c6fae97551dfcd9102d2462b0281dc4975
Terry Song <Terry Song>
2016-07-31 18:09:51 -0400

    added ball rolling

---
+++ Unity/Bowl-Master/BowlMaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    public float launchSpeed;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        LaunchBall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Launch the bowling ball
+    public void LaunchBall()
+    {
+        rigidBody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+}
